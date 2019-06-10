cb906682285775bfd29f53caae6e855e57eb9010
lsb918 <lsb918@uos.ac.kr>
2019-06-10 14:18:53 +0900

    1.0.0

--- CV_analysis.py
+++ CV_analysis.py
@@ -3,97 +3,102 @@ import re
 import time
 import matplotlib.pyplot as plt
 import pandas as pd
-#import numpy as np
 
-files = [f for f in listdir('.') if re.match('py(.*)+\.xlsx',f)]
-# print(files)
-
-# rename it
-if len(files)==0:
-    print('There aren\'t any target files. Put \'py\' infront of the name of target files')
-    time.sleep(2)
-else:
-    plots=[]
-    for f in files:
-        df=pd.read_excel(f)[1:]
+def get_cycle(f):
+    df=pd.read_excel(f)[1:]
         
-        if not 'Potential' in df.columns[0]:
-            print('You should copy datas to A1')
+    if not 'Potential' in df.columns[0]:
+        print('You should copy datas to A1')
 
-        df.columns=['potential','current']
-        x_scale=[ min(df['potential']), max(df['potential']) ]
+    df.columns=['potential','current']
+    x_scale=[ min(df['potential']), max(df['potential']) ]
 
-        # x=[]
-        # y=[]
-        # dydx=[]
-        # d2ydx2=[]
+    moving = 1 # 1,2 : ->, 3,4 : <-
+    cycle=[]
+    cycle_number=0
 
-        moving = 1 # 1 : ->, -1 : <-
-        cycle=[]
-        cycle_number=0
+    for i,row in df.iterrows():
+        if moving==1 and x_scale[1]*0.985<row[0]: # -> entered max area
+            moving=2
+        elif moving==2 and x_scale[1]*0.985>row[0]: # -> passed max area
+            moving=3
+        elif moving==3 and row[0]*0.985<x_scale[0]: # -> entered min area
+            moving=4
+        elif moving==4 and row[0]*0.985>x_scale[0]: # -> passed min area
+            moving=1
+            cycle.append(i-1)
 
-        for i,row in df.iterrows():
-            # x.append(row[0])
-            # y.append(row[1])
+    if moving==4:
+        cycle.append(len(df)-1)
+    cycle_number=len(cycle)
+    print('cycle number : %d'%cycle_number)
 
-            # if i>1:
-            #     dydx.append( (y[i-1]-y[i-2])/(x[i-1]-x[i-2]) )
+    return df,cycle,cycle_number
+
+doing=True
+while doing:
+    files = [f for f in listdir('.') if re.match('py(.*)+\.xlsx',f)]
+    # print(files)
+
+    if len(files)==0:
+        print('There aren\'t any target files. Put \'py\' infront of the name of target files')
+        time.sleep(2)
+        break
+
+    else:
+        flag=True if len(files)==1 else False
+
+        plots=[]
+        for f in files:
+            df,cycle,cycle_number = get_cycle(f)
+
+            start=0
+            for i,x in enumerate(cycle):
+                if flag:
+                    plt.plot(df['potential'][start:x],df['current'][start:x])
+
+                min_current_idx=df['current'][start:x].idxmin()
+                min_current=min(df['current'][start:x])
+
+                max_current_idx=df['current'][start:x].idxmax()
+                max_current=max(df['current'][start:x])
                 
-            # if i>2:
-            #     d2ydx2.append( (dydx[i-2]-dydx[i-3])/(x[i-2]-x[i-3]) )
+                print('Cycle #%d'%(i+1))
+                print('ia/ic : %.4f'%(abs(max_current/min_current))) #max/min
 
-            if moving==1 and x_scale[1]*0.985<row[0]: # -> entered max area
-                moving=2
-            elif moving==2 and x_scale[1]*0.985>row[0]: # -> passed max area
-                moving=3
-            elif moving==3 and row[0]*0.985<x_scale[0]: # -> entered min area
-                moving=4
-            elif moving==4 and row[0]*0.985>x_scale[0]: # -> passed min area
-                moving=1
-                cycle.append(i-1)
+                max_potential = df['potential'].iloc[max_current_idx]
+                min_potential = df['potential'].iloc[min_current_idx]
+                print('delta E : %.4f'%(max_potential-min_potential))
+                print('')
 
-        if moving==4:
-            cycle.append(len(df)-1)
-        cycle_number=len(cycle)
-        print('cycle number : %d'%cycle_number)
-        # df['dydx'] = pd.Series(dydx)
-        # df['d2ydx2'] = pd.Series(d2ydx2)
-        # df = df.assign(dydx=dydx, d2ydx2=d2ydx2)
-        # df['dydx']=df['dydx'].replace(np.inf,99)
-        # df['d2ydx2']=df['d2ydx2'].replace(-np.inf,-99)
-
-        start=0
-        for i,x in enumerate(cycle):
-            # plt.plot(df['potential'][start:x],df['current'][start:x])
-            min_current_idx=df['current'][start:x].idxmin()
-            min_current=min(df['current'][start:x])
-
-            max_current_idx=df['current'][start:x].idxmax()
-            max_current=max(df['current'][start:x])
+                start=x
             
-            print('Cycle #%d'%(i+1))
-            print('ia/ic : %.4f'%(abs(max_current/min_current))) #max/min
+            print(cycle)
+            # print(cycle_number)
+            print('##### File : %s finished. #####\n\n'%f[2:])
+            
+            # labeling method
+            try:
+                label=f[2:-5].split('(')[1].split(')')[0]
+            except:
+                label=f[2:-5].split('-')[0].split(' ')[0]
+            
+            # tmplot : last cycle's data
+            tmplot, = plt.plot(df['potential'][cycle[cycle_number-2]:cycle[cycle_number-1]], df['current'][cycle[cycle_number-2]:cycle[cycle_number-1]],label=label)
+            plots.append(tmplot)
+            
+            rename(f,f[2:])
 
-            max_potential = df['potential'].iloc[max_current_idx]
-            min_potential = df['potential'].iloc[min_current_idx]
-            print('delta E : %.4f'%(max_potential-min_potential))
-            print('')
+        # drawing graph of all 'py*.xlsx' files
+        
+        if flag:
+            plt.title('CV of %d cycles'%cycle_number)
+            plt.show()
+        else:
+            plt.title('CV of last cycle')
+            plt.legend(handles=plots)
+            plt.show()
 
-            start=x
-        
-        print(cycle)
-        # print(cycle_number)
-        print('##### File : %s finished. #####\n\n'%f[2:])
-        label=f[2:-5].split('-')[0].split(' ')[0].split('(')[1][:-1]
-        # print()
-        tmplot, =plt.plot(df['potential'][cycle[cycle_number-2]:cycle[cycle_number-1]], df['current'][cycle[cycle_number-2]:cycle[cycle_number-1]],label=label)
-        plots.append(tmplot)
-        # plt.plot(df['potential'][cycle[1]:cycle[2]],df['current'][cycle[1]:cycle[2]])
-        # plt.plot(df['potential'][cycle[1]:cycle[2]],df['d2ydx2'][cycle[1]:cycle[2]])
-        # plt.show()
-        
-        rename(f,f[2:])
-    plt.title('CV of last cycle')
-    plt.legend(handles=plots)
-    plt.show()
-    input('press enter to continue...')
+        doit=input('press enter to exit, \'r\' to calc again')
+        if doit!='r':
+            break
